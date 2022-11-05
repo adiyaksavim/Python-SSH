da0798c67f282b80517f1a7958c6d5b45d055a3a
adiyaksavim <109839230+adiyaksabackend@users.noreply.github.com>
2022-11-05 07:19:15 +0700

    Delete pythonbackend directory

--- pythonbackend/akun.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE akun(
-    id int not null auto_increment, 
-    username varchar(75) not null, 
-    email varchar(85) not null,
-    password varchar(75),
-    waktu_buat datetime default current_timestamp,
-    primary key(id)
-)
